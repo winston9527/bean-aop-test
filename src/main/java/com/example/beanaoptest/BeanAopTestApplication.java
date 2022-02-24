205d45c1df89f329f0e4eb49d7680b890b5a74b2
wchen <winston.chen@activenetwork.com>
2022-02-24 13:03:08 +0800

    commit code

--- src/main/java/com/example/beanaoptest/BeanAopTestApplication.java
+++ src/main/java/com/example/beanaoptest/BeanAopTestApplication.java
@@ -25,6 +25,25 @@ public class BeanAopTestApplication {
 		AccountService accountService2 = (AccountService)run.getBean("accountService2");
 		accountService2.sayHello("tom2");
 		System.out.println(accountService2.getClass());
+
+
+		/**
+		 *
+		 * CustomBeanDefinitionPostProcessor...postProcessBeanDefinitionRegistry
+		 * CustomBeanDefinitionPostProcessor...postProcessBeanFactory
+		 * init....
+		 * init....
+		 * beanDefinitionCount:42
+		 * aop in ...tom
+		 * 2022-02-24 12:59:08.314  INFO 14519 --- [           main] c.e.beanaoptest.BeanAopTestApplication   : Started BeanAopTestApplication in 15.508 seconds (JVM running for 15.796)
+		 * hi---tom
+		 * hi---tom1
+		 * class com.example.beanaoptest.service.AccountService$$EnhancerBySpringCGLIB$$55a29959
+		 * hi---tom2
+		 * class com.example.beanaoptest.service.AccountService
+		 */
+
+		// according to log above ,we can see that a bean register by beanFactory.registerSingleton will not generate aop proxy
 	}
 
 }
