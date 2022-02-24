6454c2d65350a83d1f653922b32548ed67d9150c
wchen <winston.chen@activenetwork.com>
2022-02-24 10:28:24 +0800

    first commit

---
+++ src/main/java/com/example/beanaoptest/BeanAopTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanaoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanAopTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanAopTestApplication.class, args);
+	}
+
+}
